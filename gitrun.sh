ab6f3b335ce8f7b9f9ce7be0e54a6ee6583803d9
David Martínez <dmg895@alumnos.unican.es>
2016-12-15 18:26:11 +0100

    Creado cfg para hvm

--- gitrun.sh
+++ gitrun.sh
@@ -5,7 +5,12 @@ read comentario
 echo "Copiando ficheros al repositorio"
 cp /etc/xen/* /git/etc/xen/
 cp /etc/default/grub /git/etc/default/
-cp ./gitrun.sh /git/
+
+# SPECweb
+cp /home/SPECweb2005/wafgen/unix/support_image_props.rc /git/ /git/SPECweb/wafgen/unix/
+cp /home/SPECweb2005/wafgen/unix/support_downloads_props.rc /git/SPECweb/wafgen/unix/
+cp /home/SPECweb2005/Test.* /git/SPECweb/
+#cp ./gitrun.sh /git/
 
 cd /git
 git init
